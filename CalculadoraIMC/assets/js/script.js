d4ace8afe7c32984e138f4b63e277c15fb19d05b
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-11-04 15:18:57 -0300

    update

--- CalculadoraIMC/assets/js/script.js
+++ CalculadoraIMC/assets/js/script.js
@@ -1,36 +1,67 @@
 const inputWeight = document.querySelector("#input-weight");
 const inputheight = document.querySelector("#input-height");
 const form = document.querySelector("form");
+const card = document.querySelector(".card-formulario");
+let exibicao = false;
 
 form.addEventListener("submit",(ev)=>{
     ev.preventDefault();
-    const weight = inputWeight.value;
-
-    const height = inputheight.value;
 
 
+    if(!exibicao){
+        const valueimc = 50;
+        let stringimc = "teste";
+    
+        const result = createIMCResult(valueimc,stringimc);
+    
+        card.appendChild(result);
+
+        exibicao = true;
+    }else{
+        const result = document.querySelector(".infos");
+
+        card.removeChild(result);
+        exibicao = false;
+    }
+    
 
 })
 
-function createIMCResult(valueIMC){
-    const body = document.querySelector("body");
+function createIMCResult(valueIMC,stringImc){
+    const divInfos = createDiv("infos");
+    const divResult = createDiv("result");
+    divInfos.appendChild(divResult);
 
-    const divInfos = document.createElement("div");
-    div.classList.add("infos");
+    const divResultValues = createDiv("result-values");
+    const spanValue = createSpan("result-value",valueIMC);
+    const spanResultImc = createSpan("result-seuImc","Seu IMC");
+    divResultValues.append(spanValue,spanResultImc);
 
-    const divResult = document.createElement("div");
-    div.classList.add("result");
+    divResult.appendChild(divResultValues);
+    const spanResultText = createSpan("result-text",stringImc);
+    divResult.appendChild(spanResultText);
 
-    const divResultValues = document.createElement("div");
-    div.classList.add("result-values");
+    const a = document.createElement("a");
+    a.classList.add("infos-ancora");
+    a.href = "#";
+    a.innerHTML = 'Mais informações sobre IMC<i class="fa-solid fa-arrow-up-right-from-square"></i>'
 
-    const pValue = document.createElement("p");
-    pValue.classList.add("result-value");
+    divInfos.appendChild(a);
 
-    const pIMC = document.createElement("p");
-    pValue.classList.add("result-seuImc");
+    return divInfos;
+}
 
-    
+function createDiv(classeName){
+    const div = document.createElement("div");
+    div.classList.add(classeName);
 
+    return div;
+}
 
+function createSpan(classeName,value){
+    const span = document.createElement("span");
+    span.classList.add(classeName);
+    span.textContent = value;
+
+    return span;
 }
